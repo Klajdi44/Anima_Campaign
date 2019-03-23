a45fdb1279e5e503a8b5ce0b8cc94921c50e9b14
Klajdi Ajdini <klajdiajdini@LAPTOP-6HS68DPE.localdomain>
2019-03-24 00:35:16 +0100

    preloader added

--- script.js
+++ script.js
@@ -15,3 +15,14 @@ for (let i=0;i<flipCards.length;i+=1){
    */
 }
     }
+function preloader(secs){
+    if (secs >= 4) {
+      document.getElementById("overlay").style.display = "none";
+    } else {
+      setTimeout(function(){preloader(secs+1);},1000);
+    }
+  }
+  window.onload = preloader(0);
+
+
+
