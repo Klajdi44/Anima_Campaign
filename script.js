53fcc0517776e47b027555e190b6239a180f83b3
Klajdi Ajdini <klajdiajdini@LAPTOP-6HS68DPE.localdomain>
2019-03-23 14:23:22 +0100

    tests done test branch

---
+++ script.js
@@ -0,0 +1,21 @@
+function displayCards(){
+let flipCards = document.getElementsByClassName('card');
+for (let i=0;i<flipCards.length;i+=1){
+  flipCards[i].style.display = 'block';
+   /*  if(flipCards.style.display === "block"){
+    flipCards.style.display = "none";
+}
+    else{
+    flipCards.style.display = "block";
+
+}
+
+
+}
+   */
+}
+    }
+
+
+
+
