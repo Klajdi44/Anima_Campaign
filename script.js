a044fc2168916deba94b208e545b926dc46a5f71
Klajdi Ajdini <klajdiajdini@LAPTOP-6HS68DPE.localdomain>
2019-03-26 23:38:50 +0100

    added modal

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let flipCards = document.getElementsByClassName('card');
 for (let i=0;i<flipCards.length;i+=1){
   flipCards[i].style.display = 'block';                     */
-   /*  if(flipCards.style.display === "block"){
+/*  if(flipCards.style.display === "block"){
     flipCards.style.display = "none";
 }
     else{
@@ -19,15 +19,46 @@ for (let i=0;i<flipCards.length;i+=1){
 */
 
 
-
-function preloader(secs){
+function preloader(secs) {
     if (secs >= 3) {
-      document.getElementById("overlay").style.display = "none";
+        document.getElementById("overlay").style.display = "none";
     } else {
-      setTimeout(function(){preloader(secs+1);},1000);
+        setTimeout(function () {
+            preloader(secs + 1);
+        }, 1000);
     }
-  }
-  window.onload = preloader(0);
+}
+window.onload = preloader(0);
+
+
+////////////////////////////////////////////////
+
+var modal = document.getElementById("simpleModal");
+///
+var modalTrigger = document.getElementById("dishes0");
+///
+var closeBtn = document.getElementsByClassName("closeBtn")[0];
+///
+modalTrigger.addEventListener("click", openModal);
+///
+closeBtn.addEventListener("click", closeModal);
+///
+window.addEventListener("click", outsideClick);
+///
+function openModal(){
+modal.style.display = "block";
+}
+///
+function closeModal(){
+modal.style.display = "none";
+}
+///
+function outsideClick(e){
+if(e.target == modal){
+    modal.style.display = "none";
+}
+}
+
 
 
 
