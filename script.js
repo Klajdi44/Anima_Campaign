19cc3bfba141eec93a8e77d8a8bfe7dde2eb8459
Klajdi Ajdini <klajdiajdini@desktop-vuhaari.kea.local>
2019-03-26 11:34:52 +0100

    changed stuff after the think aloud test

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function displayCards(){
+/*     function displayCards(){
 let flipCards = document.getElementsByClassName('card');
 for (let i=0;i<flipCards.length;i+=1){
-  flipCards[i].style.display = 'block';
+  flipCards[i].style.display = 'block';                     */
    /*  if(flipCards.style.display === "block"){
     flipCards.style.display = "none";
 }
@@ -12,11 +12,16 @@ for (let i=0;i<flipCards.length;i+=1){
 
 
 }
-   */
+                    */
+/*
 }
     }
+*/
+
+
+
 function preloader(secs){
-    if (secs >= 4) {
+    if (secs >= 3) {
       document.getElementById("overlay").style.display = "none";
     } else {
       setTimeout(function(){preloader(secs+1);},1000);
